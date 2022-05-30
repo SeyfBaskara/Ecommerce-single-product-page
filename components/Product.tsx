45d99ce3968b82f4951a644f164070518a8a0dd5
Seyf <seyfettin.baskara@gmail.com>
2022-05-30 11:42:20 +0200

    add data content

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,12 +1,46 @@
 import React, { useState, useRef } from 'react'
 import Image from 'next/image'
 
+const data = {
+   companyName: 'sneaker company',
+   title: 'Fall Limited Edition Sneakers',
+   description:
+      'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they`ll withstand everything the weather can offer.',
+   actualPrice: 250.0,
+   discountPrice: 125.0,
+   discountRate: '50%',
+   count: 0,
+}
+
+interface Iproduct {
+   companyName: string
+   title: string
+   description: string
+   actualPrice: number
+   discountPrice: number
+   discountRate: string
+   count: number
+}
+
 const Product: React.FC = () => {
+   const [product, setProduct] = useState<Iproduct>(data)
    const [slideIndex, setSlideIndex] = useState<number>(1)
    const slides = useRef<HTMLDivElement>(null)
 
    const handleSlides = () => {
       console.log(slides)
+   }
+
+   const handleProducNumber = (operator: string) => {
+      if (operator === '+') {
+         setProduct({ ...product, count: product.count + 1 })
+      } else if (product.count !== 0 && operator === '-') {
+         setProduct({ ...product, count: product.count - 1 })
+      }
+   }
+
+   const handleAddToCart = () => {
+      console.log('add to cart')
    }
 
    return (
@@ -36,28 +70,25 @@ const Product: React.FC = () => {
 
          <article className="product__details">
             <hgroup>
-               <h4 className="product__company-name">sneaker company</h4>
-               <h1 className="product__title">Fall Limited Edition Sneakers</h1>
+               <h4 className="product__company-name">{product.companyName}</h4>
+               <h1 className="product__title">{product.title}</h1>
             </hgroup>
-            <p className="product__description">
-               These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll
-               withstand everything the weather can offer.
-            </p>
+            <p className="product__description">{product.description}</p>
             <article className="product__prices">
                <h1 className="product__price">
-                  $125.00 <span className="product__discount">50%</span>
+                  ${product.discountPrice} <span className="product__discount">{product.discountRate}</span>
                </h1>
                <p className="product__prev-price">
-                  <s>$250.00</s>
+                  <s>${product.actualPrice}</s>
                </p>
             </article>
             <article>
                <div className="product__number-set">
-                  <Image src="/images/icon-minus.svg" alt="minus" width={12} height={4} />
-                  <p>0</p>
-                  <Image src="/images/icon-plus.svg" alt="plus" width={12} height={12} />
+                  <Image onClick={() => handleProducNumber('-')} src="/images/icon-minus.svg" alt="minus" width={12} height={4} />
+                  <p>{product.count}</p>
+                  <Image onClick={() => handleProducNumber('+')} src="/images/icon-plus.svg" alt="plus" width={12} height={12} />
                </div>
-               <button className="product__add-btn">
+               <button onClick={handleAddToCart} className="product__add-btn">
                   <div className="product__btn-icon">
                      <Image src="/images/icon-cart.svg" alt="cart" width={18} height={16} />
                   </div>
